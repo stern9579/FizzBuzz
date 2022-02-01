6c6309bcb003ad1f57817961ebd1f629cf63ac04
Andrew Stern <andrewdstern16@gmail.com>
2022-01-31 17:43:06 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+for (var x = 1; x<101; x++) {
+    if (x % 3 == 0 && x % 5 ==0) {
+        console.log("FizzBuzz");
+    }
+    else if (x % 5 == 0) {
+        console.log("Buzz");
+    }
+    else if (x % 3 == 0) {
+        console.log("Fizz");
+    }
+    else {
+        console.log(x);
+    }
+}
